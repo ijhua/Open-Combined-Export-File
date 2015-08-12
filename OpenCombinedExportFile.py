9ce4e90998ad26fd9cf6d4e836af70afc8a28943
ijhua <hua.isabelle@gmail.com>
2015-08-12 10:52:17 -0700

    Update OpenCombinedExportFile.py
    This code opens the combined export file and prints only the sleep data. Note that the csv file name has been changed.

--- OpenCombinedExportFile.py
+++ OpenCombinedExportFile.py
@@ -5,11 +5,11 @@ import itertools
 Choose the range of the rows in the parentheses.
 Remember: with an index, you have to subtract 1'''
 
-#delete intertools.islice to print entire file with no row restrictions
+#add itertools.islice to print entire file with row restrictions
 
 with open('Combined_Export_File.csv') as csvfile:
 	readCSV = csv.reader(csvfile, dialect='excel')
 #choose the columns that are printed. remember indexes!
 	for column in readCSV:
-		if "SLEEP" in column: #limit the printing to sleep
+		if "SLEEP" in column: #limit the printing to sleep, does not print the headers
 			print (column)
