10dc6836e30564cb0fba849827931308530533bd
ijhua <hua.isabelle@gmail.com>
2015-08-13 16:31:34 -0700

    Update OpenWatchFilePandas.py
    The code no longer needs to be set to skip a number of lines, but I'm having trouble with making the boolean so that all of the strings in Interval Status are included. Right now, only "ACTIVE" is printing. I want "EXCLUDED", "REST", "REST-S" to print.

--- OpenWatchFilePandas.py
+++ OpenWatchFilePandas.py
@@ -2,12 +2,14 @@
 import pandas as pd
 import numpy as np
 
+#define dataframe
+#df=DataFrame("Subject_01_04_03_2015_9_00_00_AM_New_Analysis.csv")
+
 #name columns
 my_columns=["Line","Date","Time","Off-Wrist Status","Activity","Marker","White Light","Red Light","Green Light","Blue Light","Sleep/Wake","Interval Status"]
 
-#print the csv
-print (pd.read_csv("Subject_01_04_03_2015_9_00_00_AM_New_Analysis.csv", skiprows=153, index_col=-1, names=my_columns, engine='python'))
+#define parts of the csv
+file= pd.read_csv("Subject_01_04_03_2015_9_00_00_AM_New_Analysis.csv", names=my_columns)
 
-'''
-This code can open an export file. I set it to skip the first 153 lines, which is hopefully the same number in each subject export file. When it prints the csv, the columns are sometimes put underneath, probably because there is a limit on the screen size. I don't know what impact this will have in the future when we try to join csv files together. 
-'''
+#print the csv
+print (file[file["Interval Status"]=="ACTIVE"])
