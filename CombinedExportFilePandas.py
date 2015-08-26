220fd39f267a828bd9d4caf9a9c9839a926cd1c9
ijhua <hua.isabelle@gmail.com>
2015-08-26 10:48:15 -0700

    Update CombinedExportFilePandas.py
    Now identifies weekend/weekday and labels them. weekend=1 weekday=0. Also resets the index to number the rows without skipping.

--- CombinedExportFilePandas.py
+++ CombinedExportFilePandas.py
@@ -1,7 +1,11 @@
 #set up imports
 import pandas as pd
 import numpy as np
+import os
+import datetime
 
+
+path=r'C:\Users\Isabelle\Python\Summary_Data'
 file_name="Combined_Export_File.csv"
 my_columns=["Subject ID","Data Start Date","Data Start Time",
 	"Interval Type","Interval#","Start Date","Start Time",
@@ -20,9 +24,30 @@ my_columns=["Subject ID","Data Start Date","Data Start Time",
 	"#No Responses (B)","Avg Score (B)","Avg Manual (B)",
 	"Avg Scheduled (B)","Avg Late Score (B)"]
 
-#print the csv
-file=(pd.read_csv(file_name, usecols=my_columns, engine='python'))
-print(file[file["Interval Type"]=="SLEEP"])
+#read the file into csv
+file=(pd.read_csv(path+"\\"+file_name, usecols=my_columns, engine='python'))
+#print(file[file["Interval Type"]=="SLEEP"]) #print the csv into the console
 
+#put into DataFrame
 df = pd.DataFrame(file[file["Interval Type"]=="SLEEP"])
-df.to_csv("New_Export_File.csv", encoding='utf-8')
+
+#insert a new row for weekend. default value=A, so that it's different from everything else
+df.insert(loc=4, column='weekend', value='A')
+
+# create datetime object
+enddate = pd.to_datetime(df.iloc[:,8])
+enddate = enddate.astype(datetime.date)
+
+# index on datetime
+df.index = pd.DatetimeIndex(enddate)
+weekday=df.index.weekday
+
+#define values for the weekend column. Everything has to be in one line or else it doesn't work.
+# weekend=1, weekday=0. Base on wake date. Not necessarily accurate all of the time 
+df['weekend'] = np.where((weekday==5)|(weekday==6),1,0)
+
+#reset index
+df=df.reset_index(drop=True)
+
+#to csv
+df.to_csv(path+"\\New_Export_File.csv", encoding='utf-8')
